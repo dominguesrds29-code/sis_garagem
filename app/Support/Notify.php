d7e3b85b70d6373933cb8bf1f45d0dc051d1b6fa
Zaccaro <zaccaro.bct@gmail.com>
2022-05-20 08:19:27 -0300

    Concluindo cadastro de Viaturas

---
+++ app/Support/Notify.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Support;
+
+class Notify
+{
+    
+    private $text;
+    private $actionTextColor;
+    private $backgroundColor;
+    private $pos;    
+    private $duration;
+    
+    function getText()
+    {
+        return $this->text;
+    }
+    
+    function getActionTextColor()
+    {
+        return $this->actionTextColor;
+    }
+    
+    function getBackgroundColor()
+    {
+        return $this->backgroundColor;
+    }
+    
+    function getPos()
+    {
+        return $this->pos;
+    }
+    
+    
+    public function getDuration()
+    {
+        return $this->duration;
+    }
+    
+    
+    function __construct()
+    {
+        $this->pos = 'top-right';
+        $this->actionTextColor = '#fff';
+        $this->duration = 5000;
+    }
+    
+    public function success(string $message): Notify
+    {
+        $this->text = $message;
+        $this->backgroundColor = '#1abc9c';
+        return $this;
+    }
+    
+    public function error(string $message): Notify
+    {
+        $this->text = $message;        
+        $this->backgroundColor = '#e2a03f';
+        return $this;
+    }
+    
+    public function info(string $message): Notify
+    {
+        $this->text = $message;
+        $this->backgroundColor = '#2196f3';
+        return $this;
+    }
+    
+    public function warning(string $message): Notify
+    {
+        $this->text = $message;
+        $this->backgroundColor = '#e2a03f';
+        return $this;
+    }
+    
+    public function primary(string $message): Notify
+    {
+        $this->text = $message;
+        $this->backgroundColor = '#4361ee';
+        return $this;
+    }
+    
+    public function custom(string $message, string $actionTextColor, string $backgroundColor, string $pos, int $duration): Notify
+    {
+        $this->text = $message;
+        $this->actionTextColor = $actionTextColor;
+        $this->backgroundColor = $backgroundColor;
+        $this->pos = $pos;
+        $this->duration = $duration;
+        return $this;
+    }
+    
+    public function render()
+    {
+        return [
+            'text' => $this->getText(),
+            'backgroundColor' => $this->getBackgroundColor(),
+            'actionTextColor' => $this->getActionTextColor(),
+            'pos' => $this->getPos(),
+            'duration' => $this->getDuration()
+        ];
+    }
+    
+}
