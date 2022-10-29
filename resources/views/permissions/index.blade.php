022a2e1b5d27b2269255b19fae68179551a09764
zaccaro_atc <zaccaro_atc@hotmail.com>
2022-10-28 23:07:19 -0300

    Correção na lista de Permissões e Perfis e na Lista de Usuários

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -25,7 +25,6 @@
                                 <tr>
                                     <th class="checkbox-column text-center"> ID</th>
                                     <th>Permissão</th>
-                                    <th class="text-center">Guard</th>
                                     <th class="text-center">Ações</th>
                                 </tr>
                                 </thead>
